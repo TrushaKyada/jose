e4657c2d916d409ab643fda022aa48dd817e8f2d
TrushaKyada <108670762+TrushaKyada@users.noreply.github.com>
2023-01-27 13:01:56 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/jose")
+mongoose.connect("mongodb+srv://trusha_kyada:tru%40sha098@cluster0.iyckzvk.mongodb.net/test")
     .then(() => {
         console.log("database connected successfully...");
     })
